ff03cb3b9af941c15d4e6749bdae8e36cd40c068
Zukedog <zukedog@zukedog.com>
2022-12-17 17:33:30 +1030

    first implementation of library transfering memory from controller to module

---
+++ KTANE_2.0_Controller/KTANE_2.0_Controller.ino
@@ -0,0 +1,124 @@
+#include <Wire.h>
+#include <EasyTransferI2C.h>
+#define I2C_SLAVE_ADDRESS 9
+
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+enum Game_Phase {
+  PREP,
+  GAME
+};
+
+struct GAME_STATE {
+  // This will hold the state of the game, armed, TotalStrikes, edgework, etc.
+  // This needs to be the same on all modules and the controller
+  int strikes;
+  Game_Phase phase;
+  int batteries;
+  bool parallelPort;
+  bool serialOdd;
+  bool serialVowel;
+};
+
+struct MODULE_STATE {
+  // This will hold the state of the module that is shared between modules and the controller, ModuleStrikes, FinishedStage
+  // This needs to be the same on all modules and the controller
+  int module_strikes;
+  bool finished_stage;
+  char module_name[4];
+  uint8_t private_size;
+
+};
+
+
+EasyTransferI2C ET_GAME_STATE;
+GAME_STATE game_state;
+
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  ET_GAME_STATE.begin(details(game_state), &Wire);
+  inputString.reserve(10);
+}
+void loop() {
+
+  if (stringComplete) {
+    if (inputString == "arm\n") {
+      game_state.phase = GAME;
+    }
+    if (inputString == "disarm\n") {
+      game_state.phase = PREP;
+    }
+    if (inputString == "odd\n") {
+      game_state.serialOdd = true;
+    }
+    if (inputString == "even\n") {
+      game_state.serialOdd = false;
+    }
+    if (inputString == "vowel\n") {
+      game_state.serialVowel = true;
+    }
+    if (inputString == "novowel\n") {
+      game_state.serialVowel = false;
+    }
+    if (inputString == "strike\n") {
+      game_state.strikes++;
+    }
+    ET_GAME_STATE.sendData(I2C_SLAVE_ADDRESS);
+    
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+
+
+void prep_setup() {
+
+}
+
+bool prep_loop() {
+  // This is the loop that will run when the bomb is in its prep phase
+  // Return true if the bomb is ready to be armed
+  return false;
+}
+
+
+
+void game_setup() {
+
+}
+
+bool game_loop() {
+  // This is the loop that will run when the bomb is in its game phase
+  // Return true if the bomb is ready to be armed
+  return false;
+}
+
+void disarmed() {
+
+}
+
+void win() {
+
+}
+
+void reset() {
+
+}
---
+++ KTANE_2.0_Module/KTANE_2.0_Module.ino
@@ -0,0 +1,79 @@
+#include <Wire.h>
+#include <EasyTransferI2C.h>
+#define I2C_SLAVE_ADDRESS 9
+
+
+enum Game_Phase {
+  PREP,
+  GAME
+};
+
+struct GAME_STATE {
+  // This will hold the state of the game, armed, TotalStrikes, edgework, etc.
+  // This needs to be the same on all modules and the controller
+  int strikes;
+  Game_Phase phase;
+  int batteries;
+  bool parallelPort;
+  bool serialOdd;
+  bool serialVowel;
+};
+
+struct MODULE_STATE {
+  // This will hold the state of the module that is shared between modules and the controller, ModuleStrikes, FinishedStage
+  // This needs to be the same on all modules and the controller
+  int module_strikes;
+  bool finished_stage;
+  char module_name[4];
+  uint8_t private_size;
+};
+
+
+EasyTransferI2C ET_GAME_STATE;
+GAME_STATE game_state;
+
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(I2C_SLAVE_ADDRESS);
+  //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc.
+  ET_GAME_STATE.begin(details(game_state), &Wire);
+  //define handler function on receiving data
+  Wire.onReceive(receive);
+}
+
+void loop() {
+  if (ET_GAME_STATE.receiveData()) {
+    Serial.print("Phase: ");
+    Serial.println(game_state.phase);
+    Serial.print("Strikes: ");
+    Serial.println(game_state.strikes);
+    Serial.print("Batteries: ");
+    Serial.println(game_state.batteries);
+    Serial.print("PP: ");
+    Serial.println(game_state.parallelPort);
+    Serial.print("Serial: ");
+    if (game_state.serialOdd) {
+      Serial.print("Odd, ");
+    } else {
+      Serial.print("Even, ");
+    }
+    if (game_state.serialVowel) {
+      Serial.println("Vowel ");
+    } else {
+      Serial.println("No Vowel ");
+    }
+  }
+
+}
+
+
+// HIDE EVERYTHING ABOVE THIS LINE IN A LIBRARY LATER
+
+struct PRIVATE_MODULE_STATE {
+  // This will hold the state of the module that is specific to the one module, Colour of button, Number of wires, etc.
+  // This will be different on every module including size.
+  // make sure you define arrays like this "int stuff[5];" not like this "int stuff[];" it breaks if they are dynamically assigned
+};
+
+void receive(int numBytes) {}
